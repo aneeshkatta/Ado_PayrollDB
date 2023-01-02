9cf4a979ffc17e31289cae5a5373f9a062090057
Aneesh <katta.aneesh597@gmail.com>
2023-01-02 05:33:11 +0530

    added program

---
+++ Ado_PayrollDB_Day34/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado_PayrollDB_Day34
+{
+    internal class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        public double Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public double MobileNo { get; set; }
+        public string Department { get; set; }
+        public double Deductions { get; set; }
+        public double Taxable_Pay { get; set; }
+        public double Income_Tax { get; set; }
+        public double Net_Pay { get; set; }
+    }
+}
---
+++ Ado_PayrollDB_Day34/EmployeeRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado_PayrollDB_Day34
+{
+    internal class EmployeeRepo
+    {   
+            public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Payroll_Service;Integrated Security=True";
+            public static void CheckConnection()    //UC1 Verifying the Connectivity status with the db.
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection with database has been established.");
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in Source connection. " + ex.Message);
+                }
+            }       
+    }
+}
